2435ff934b57a2b25445ccdf53e9dd97e80564e3
xexe <francesccoll@paucasesnovescifp.cat>
2020-12-05 17:37:51 +0100

    Petits cambis estetics en es filtro

--- Task1DB01/Task1DB01/Product.cs
+++ Task1DB01/Task1DB01/Product.cs
@@ -9,43 +9,17 @@ namespace Task1DB01
     public class ProductModel
     {
         public string Name { get; set; }
-        public string ProductNumber { get; set; }
-        public int MakeFlag { get; set; }
-        public int FinishedGoodsFlag { get; set; }
+        public string MakeFlag { get; set; }
+        public string ProductID { get; set; }
         public string Color { get; set; }
-        public int SafetyStockLevel { get; set; }
-        public int ReorderPoint { get; set; }
-        public int StandardCost { get; set; }
-        public int ListPrice { get; set; }
+        public string ListPrice { get; set; }
         public string Size { get; set; }
-        public string SizeUnitMeasureCode { get; set; }
-        public string WeightUnitMeasureCode { get; set; }
-        public int Weight { get; set; }
-        public int DaysToManufacture { get; set; }
         public string ProductLine { get; set; }
         public string Class { get; set; }
         public string Style { get; set; }
-        public int ProductSubcategoryID { get; set; }
-        public int ProductModelID { get; set; }
-        public string SellStartDate { get; set; }
-        public string SellEndDate { get; set; }
-        public string DiscontinuedDate { get; set; }
-        public string rowguid { get; set; }
-        public string ModifiedDate { get; set; }
+        public string ProductSubcategoryID { get; set; }
         public string ProductMOdel { get; set; }
         public string Description { get; set; }
-
-        public string FullInfo
-        {
-            get
-            {
-                return $"{ Name }  ||  { ProductNumber }  ||  { MakeFlag }  ||  { FinishedGoodsFlag }  ||  " +
-                    $"{ Color }  ||  { SafetyStockLevel }  ||  { ReorderPoint }  ||  { StandardCost }  ||  { ListPrice }  ||  " +
-                    $"{ Size }  ||  { SizeUnitMeasureCode }  ||  { WeightUnitMeasureCode }  ||  { Weight }  ||  { DaysToManufacture }  ||  " +
-                    $"{ ProductLine }  ||  { Class }  ||  { Style }  ||  { ProductSubcategoryID }  ||  { ProductModelID }  ||  { SellStartDate }  ||  " +
-                    $"{ SellEndDate }  ||  { DiscontinuedDate }  ||  { rowguid }  ||  { ModifiedDate } ";
-            }
-        }
 
         public string NamDes
         {
@@ -59,7 +33,7 @@ namespace Task1DB01
         {
             get
             {
-                return $"{ProductMOdel}  ||  {Description}  || { Size }";
+                return $"{ProductMOdel}  ||  {Description}  || { Size } { ProductID } { Name } { ProductSubcategoryID } { ProductLine } { Class } { Style } { ListPrice } { Color } { MakeFlag }";
             }
         }
     }
